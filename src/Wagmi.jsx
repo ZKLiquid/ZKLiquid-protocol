fd3a2bc5515650fbdf86c56ddd9d813b22cea663
ETNA-Project <88162641+ETNA-Project@users.noreply.github.com>
2023-09-18 08:00:30 -0400

    Update Wagmi.jsx

--- src/Wagmi.jsx
+++ src/Wagmi.jsx
@@ -19,12 +19,12 @@ import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet, polygon, bsc],
   [
+    infuraProvider({ apiKey: 'ac546be7d92f46f5bbd794a14f4fd707' }),
     jsonRpcProvider({
       rpc: (chain) => ({
         http: `https://56.https://bsc-mainnet.core.chainstack.com/ac5a7a79e0bfe4a915f59e3e39ac0f56`,
       }),
     }),
-    infuraProvider({ apiKey: 'ac546be7d92f46f5bbd794a14f4fd707' }),
     publicProvider(),
   ]
 );
